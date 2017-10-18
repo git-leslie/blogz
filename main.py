3584ea8b2f576d2c6b2f2e37f578b4158fc23a1f
Leslie <llee.design@yahoo.com>
2017-10-17 21:00:27 -0500

    added login and signup pages

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template, session, flash
-from validators import validate_title, validate_body
+from validators import validate_title, validate_body, validate_verify_pw
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,24 +9,75 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
 
-class Owner_id(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(5000))
 
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_title = db.Column(db.String(120))
+    post_body = db.Column(db.String(5000))
+
+    def __init__(self, post_title, post_body, owner):
+        self.post_title = post_title
+        self.post_body = post_body
+        self.owner = owner
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120))
     password = db.Column(db.String(120))
-    blogs = db.relationship('blogs', backref='owner')
+    posts = db.relationship('Post', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session["username"] = username
+            flash("Logged in")
+            return redirect('/newpost')
+        else:
+            flash('User password incorrect, or user does not exist')
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify_pw = request.form['verify_pw']
+
+        username_error = validate_gen(username)
+        password_error = validate_gen(password)
+        verify_pw_error = validate_verify_pw(password, verify_pw)
+
+        if username_error or password_error:
+            flash(username_error)
+            return render_template("signup.html", username=username)
+
+        if verify_pw_error:
+            flash(verify_pw_error)
+            return render_template("signup.html", username=username)
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            flash("Don't be a copycat, try a different username")
+
+    return render_template('register.html')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -54,11 +105,11 @@ def blog():
 def newpost():
 
     if request.method == 'POST':
-        posttitle = request.form['posttitle']
-        body = request.form['body']
+        post_title = request.form['post_title']
+        post_body = request.form['post_body']
 
-        title_error = validate_title(posttitle)
-        body_error = validate_body(body)       
+        title_error = validate_title(post_title)
+        body_error = validate_body(post_body)       
 
         if title_error or body_error:
             if title_error:
@@ -69,7 +120,7 @@ def newpost():
                                                  , posttitle=posttitle
                                                  , body=body)        
         else:
-            new_post = Post(posttitle, body)
+            new_post = Post(post_title, post_body)
             db.session.add(new_post)
             db.session.commit()
             #return redirect('/post')
@@ -95,6 +146,3 @@ def post():
 
 if __name__ == '__main__':
     app.run()
-
-
-    db.session.commit()
--- validators.py
+++ validators.py
@@ -1,16 +1,27 @@
 def validate_title(text):
-
     error = 0
-
     if not text:
         error = 'Add a freaking title'
     return error
 
 
 def validate_body(body):
-
     error = 0
-
     if not body:
         error = "Omg geeze did you forget the body??"
+    return error
+
+
+
+def validate_gen(text):
+    error = 0
+    if " " in text or len(text) < 3 or len(text) > 20 or text == "":
+        error = "Username and password must be between 3-20 characters, without spaces"
+    return error
+
+
+def validate_verify_pw(password, verify_pw):
+    error = 0
+    if password != verify_pw:
+        error = "Passwords do not match"
     return error
